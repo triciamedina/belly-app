a175283034034539a24762adbe59262a9bf3dfbd
triciamedina <tricia.medina@gmail.com>
2020-04-19 23:33:50 -0700

    Bill form component

--- src/components/BillForm/BillForm.js
+++ src/components/BillForm/BillForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import './BillForm.css';
-import { IconClose } from '../UI/UI';
+import { IconClose, Button } from '../UI/UI';
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
 
 function BillForm() {
     const [ shouldShowPicker, togglePickerState ] = useState();
+    const history = useHistory();
     const emojiEl = useRef(null)
 
     const selectEmojiHandler = (emoji) => {
@@ -16,7 +18,7 @@ function BillForm() {
     return (
         <>
             <header className='BillFormHeader'>
-                <button className='Close'>
+                <button className='Close' onClick={() => history.goBack()}>
                     <IconClose />
                 </button>
             </header>
@@ -25,7 +27,10 @@ function BillForm() {
                     <div className='input-container emoji'>
                         <input
                             type='text'
+                            id='emoji'
+                            name='emoji'
                             ref={emojiEl}
+                            placeholder='🌯'
                             className='emoji-input'
                             onClick={() => togglePickerState(!shouldShowPicker)} 
                             aria-label='Emoji'
@@ -34,6 +39,91 @@ function BillForm() {
                             ? <Picker onSelect={(emoji) => selectEmojiHandler(emoji.native)}/>
                             : null
                         }
+                    </div>
+                    <div className='input-container'>
+                        <input 
+                            type='text' 
+                            id='bill-name' 
+                            name='bill-name'
+                            className='name-input'
+                            placeholder='Bill name'
+                            aria-label='Bill name'
+                        />
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='discounts'>
+                            Discounts
+                        </label>
+                        <span>$</span>
+                        <input 
+                            type='number'
+                            min='0'
+                            step='.01'
+                            id='discounts' 
+                            name='discounts' 
+                            placeholder='0.00'
+                        />
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='tax'>
+                            Tax
+                        </label>
+                        <span>$</span>
+                        <input 
+                            type='number'
+                            min='0'
+                            step='.01'
+                            id='tax' 
+                            name='tax' 
+                            placeholder='0.00'
+                        />
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='tip'>
+                            Tip
+                        </label>
+                        <span>$</span>
+                        <input 
+                            type='number'
+                            min='0'
+                            step='.01'
+                            id='tip' 
+                            name='tip' 
+                            placeholder='0.00'
+                        />
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='fees'>
+                            Fees
+                        </label>
+                        <span>$</span>
+                        <input 
+                            type='number'
+                            min='0'
+                            step='.01'
+                            id='fees' 
+                            name='fees' 
+                            placeholder='0.00'
+                        />
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='total'>
+                            Total
+                        </label>
+                        <span>$</span>
+                        <input 
+                            type='number'
+                            min='0'
+                            step='.01'
+                            id='total' 
+                            name='total' 
+                            placeholder='0.00'
+                        />
+                    </div>
+                    <div className='button-container'>
+                        <Button className='Button' type='submit'>
+                            Next
+                        </Button>
                     </div>
                 </form>
             </main>
