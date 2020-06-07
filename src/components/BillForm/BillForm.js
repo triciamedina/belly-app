f217d406570b3653df0e02901d014747eb485ef5
triciamedina <tricia.medina@gmail.com>
2020-06-06 17:22:20 -0700

    Bill form styling

--- src/components/BillForm/BillForm.js
+++ src/components/BillForm/BillForm.js
@@ -113,163 +113,161 @@ function BillForm(props) {
     }
 
     return (
-        <>
+        <div className='isolate'>
+            <div className='BillFormGradient radial'></div>
+            <div className='BillFormGradient linear'></div>
             <header className='BillFormHeader'>
                 <div className='BillFormHeader__container'>
                     <ButtonClose className='Close' color='white' onClick={closeHandler}/>
                 </div>
             </header>
-            <div className='isolate'>
-                <div className='gradient radial'></div>
-                <div className='gradient linear'></div>
-                <main className='BillFormContainer'>
-                    <form className='BillForm' onSubmit={event => submitHandler(event)}>
-                        <div className='input-container emoji'>
-                            <label htmlFor='emoji'>Emoji</label>
-                            <input
-                                type='text'
-                                id='emoji'
-                                name='emoji'
-                                ref={emojiEl}
-                                placeholder='🌯'
-                                className='emoji-input'
-                                onFocus={() => togglePickerState(!shouldShowPicker)} 
-                                aria-label='Emoji'
-                                defaultValue={existingBill ? existingBill.bill_thumbnail : ''}
-                                required
-                            />
-                            {shouldShowPicker
-                                ? <Picker onSelect={(emoji) => selectEmojiHandler(emoji.native)}/>
-                                : null
-                            }
-                        </div>
-                        <div className='input-container'>
-                            <label htmlFor='bill-name'>Bill name</label>
+            <main className='BillFormContainer'>
+                <form className='BillForm' onSubmit={event => submitHandler(event)}>
+                    <div className='input-container emoji'>
+                        <label htmlFor='emoji'>Pick an emoji</label>
+                        <input
+                            type='text'
+                            id='emoji'
+                            name='emoji'
+                            ref={emojiEl}
+                            placeholder='🌯'
+                            className='emoji-input'
+                            onFocus={() => togglePickerState(!shouldShowPicker)} 
+                            aria-label='Emoji'
+                            defaultValue={existingBill ? existingBill.bill_thumbnail : ''}
+                            required
+                        />
+                        {shouldShowPicker
+                            ? <Picker onSelect={(emoji) => selectEmojiHandler(emoji.native)}/>
+                            : null
+                        }
+                    </div>
+                    <div className='input-container name'>
+                        <label htmlFor='bill-name'>Bill name</label>
+                        <input 
+                            type='text' 
+                            id='bill-name' 
+                            name='bill-name'
+                            className='name-input'
+                            placeholder='Bill name'
+                            aria-label='Bill name'
+                            value={enteredBillName}
+                            onChange={event => onBillNameChange(event.target.value)}
+                        />
+                        {billNameTouched &&
+                            (<Error
+                                className='Error billname'
+                                message={ValidationService.validateBillName(enteredBillName)} 
+                            />)
+                        }
+                    </div>
+                    <div className='input-container currency'>
+                        <label htmlFor='tax'>
+                            Tax
+                        </label>
+                        <div className='box'>
+                            <span>$</span>
                             <input 
-                                type='text' 
-                                id='bill-name' 
-                                name='bill-name'
-                                className='name-input'
-                                placeholder='Bill name'
-                                aria-label='Bill name'
-                                value={enteredBillName}
-                                onChange={event => onBillNameChange(event.target.value)}
+                                type='number'
+                                min='0'
+                                step='.01'
+                                id='tax' 
+                                name='tax'
+                                value={enteredTax}
+                                onChange={event => onTaxChange(event.target.value)}
                             />
-                            {billNameTouched &&
-                                (<Error
-                                    className='Error billname'
-                                    message={ValidationService.validateBillName(enteredBillName)} 
-                                />)
+                        </div>
+                    </div>
+                    {taxTouched &&
+                        (<Error 
+                            message={ValidationService.validateTax(enteredTax)} 
+                        />)
+                    }
+                    <div className='input-container currency'>
+                        <label htmlFor='tip'>
+                            Tip
+                        </label>
+                        <div className='box'>
+                            <span>$</span>
+                            <input 
+                                type='number'
+                                min='0'
+                                step='.01'
+                                id='tip' 
+                                name='tip'
+                                value={enteredTip}
+                                onChange={event => onTipChange(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                    {tipTouched &&
+                        (<Error 
+                            message={ValidationService.validateTip(enteredTip)} 
+                        />)
+                    }
+                    <div className='input-container currency'>
+                        <label htmlFor='fees'>
+                            Fees
+                        </label>
+                        <div className='box'>
+                            <span>$</span>
+                            <input 
+                                type='number'
+                                min='0'
+                                step='.01'
+                                id='fees' 
+                                name='fees'
+                                value={enteredFees}
+                                onChange={event => onFeesChange(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                    {feesTouched &&
+                        (<Error 
+                            message={ValidationService.validateFees(enteredFees)} 
+                        />)
+                    }
+                    <div className='input-container currency'>
+                        <label htmlFor='discounts'>
+                            Discounts
+                        </label>
+                        <div className='box'>
+                            <span>$</span>
+                            <input 
+                                type='number'
+                                min='0'
+                                step='.01'
+                                id='discounts' 
+                                name='discounts'
+                                value={enteredDiscounts}
+                                onChange={event => onDiscountsChange(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                    {discountsTouched &&
+                        (<Error 
+                            message={ValidationService.validateDiscounts(enteredDiscounts)} 
+                        />)
+                    }
+                    <div className='button-container'>
+                        {submitError && (<Error className='Error submit' message={submitError} />) }
+                        <Button 
+                            className='Button' 
+                            type='submit'
+                            disabled={
+                                ValidationService.validateBillName(enteredBillName)
+                                || ValidationService.validateDiscounts(enteredDiscounts)
+                                || ValidationService.validateTax(enteredTax)
+                                || ValidationService.validateTip(enteredTip)
+                                || ValidationService.validateFees(enteredFees)
                             }
-                        </div>
-                        <div className='input-container currency'>
-                            <label htmlFor='tax'>
-                                Tax
-                            </label>
-                            <div className='box'>
-                                <span>$</span>
-                                <input 
-                                    type='number'
-                                    min='0'
-                                    step='.01'
-                                    id='tax' 
-                                    name='tax'
-                                    value={enteredTax}
-                                    onChange={event => onTaxChange(event.target.value)}
-                                />
-                            </div>
-                            {taxTouched &&
-                                (<Error 
-                                    message={ValidationService.validateTax(enteredTax)} 
-                                />)
-                            }
-                        </div>
-                        <div className='input-container currency'>
-                            <label htmlFor='tip'>
-                                Tip
-                            </label>
-                            <div className='box'>
-                                <span>$</span>
-                                <input 
-                                    type='number'
-                                    min='0'
-                                    step='.01'
-                                    id='tip' 
-                                    name='tip'
-                                    value={enteredTip}
-                                    onChange={event => onTipChange(event.target.value)}
-                                />
-                            </div>
-                            {tipTouched &&
-                                (<Error 
-                                    message={ValidationService.validateTip(enteredTip)} 
-                                />)
-                            }
-                        </div>
-                        <div className='input-container currency'>
-                            <label htmlFor='fees'>
-                                Fees
-                            </label>
-                            <div className='box'>
-                                <span>$</span>
-                                <input 
-                                    type='number'
-                                    min='0'
-                                    step='.01'
-                                    id='fees' 
-                                    name='fees'
-                                    value={enteredFees}
-                                    onChange={event => onFeesChange(event.target.value)}
-                                />
-                            </div>
-                            {feesTouched &&
-                                (<Error 
-                                    message={ValidationService.validateFees(enteredFees)} 
-                                />)
-                            }
-                        </div>
-                        <div className='input-container currency'>
-                            <label htmlFor='discounts'>
-                                Discounts
-                            </label>
-                            <div className='box'>
-                                <span>$</span>
-                                <input 
-                                    type='number'
-                                    min='0'
-                                    step='.01'
-                                    id='discounts' 
-                                    name='discounts'
-                                    value={enteredDiscounts}
-                                    onChange={event => onDiscountsChange(event.target.value)}
-                                />
-                            </div>
-                            {discountsTouched &&
-                                (<Error 
-                                    message={ValidationService.validateDiscounts(enteredDiscounts)} 
-                                />)
-                            }
-                        </div>
-                        <div className='button-container'>
-                            {submitError && (<Error className='Error submit' message={submitError} />) }
-                            <Button 
-                                className='Button' 
-                                type='submit'
-                                disabled={
-                                    ValidationService.validateBillName(enteredBillName)
-                                    || ValidationService.validateDiscounts(enteredDiscounts)
-                                    || ValidationService.validateTax(enteredTax)
-                                    || ValidationService.validateTip(enteredTip)
-                                    || ValidationService.validateFees(enteredFees)
-                                }
-                            >
-                                {existingBill ? 'Save' : 'Next'}
-                            </Button>
-                        </div>
-                    </form>
-                </main>
-            </div>
-        </>
+                        >
+                            {existingBill ? 'Save' : 'Next'}
+                        </Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
 
