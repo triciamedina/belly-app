f2689b2690a08075fab11bfc240ac9013353e547
triciamedina <tricia.medina@gmail.com>
2020-06-08 07:52:30 -0700

    Add test suites to UI tests

--- src/components/UI/UI.test.js
+++ src/components/UI/UI.test.js
@@ -18,87 +18,114 @@ import {
     IconUsers
 } from './UI';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<Button />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Button />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonLink />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonLink />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonLink />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TextLink />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<TextLink />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<TextLink />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Header />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<Header />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Header />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonBack />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonBack />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonBack />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonClose />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonClose />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonClose />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonMenu />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonMenu />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonMenu />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonMore />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonMore />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonMore />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonShare />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonShare />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonShare />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonDown />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonDown />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonDown />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ButtonUp />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<ButtonUp />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ButtonUp />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Emoji />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<Emoji />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Emoji />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<IconEdit />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<IconEdit />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<IconEdit />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<IconUsers />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('<IconUsers />', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<IconUsers />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
