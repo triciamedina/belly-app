9f21ab29f4ed6fd6ecbfee1019d4580858df1d18
triciamedina <tricia.medina@gmail.com>
2020-06-08 09:06:22 -0700

    BillTotals test suite

--- src/components/BillTotals/BillTotals.js
+++ src/components/BillTotals/BillTotals.js
@@ -6,70 +6,73 @@ import { calculateBillSubtotal } from '../../lib/split';
 const Money = Dinero;
 const currency = 'USD';
 
-
 function BillTotals(props) {
     const { currentBill } = props;
 
-    const subtotal = calculateBillSubtotal(currentBill);
-    const discounts = Money({ amount: (Number(currentBill.discounts)*100), currency});
-    const tax = Money({ amount: (Number(currentBill.tax)*100), currency});
-    const tip = Money({ amount: (Number(currentBill.tip)*100), currency});
-    const fees = Money({ amount: (Number(currentBill.fees)*100), currency});
-    const total = subtotal.add(tax).add(tip).add(fees).subtract(discounts);
-    
-    return (
-        <div className='bill-totals'>
-            <div className='col-1'>
-                <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
-                    <h2>
-                        Discounts
-                        <span className='currency'>$</span>
-                        <span className='amount'>
-                            {discounts.toFormat('0,0.00')}
-                        </span>
-                    </h2>
-                </Link>
-            </div>
-            <div className='col-2'>
-                <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
-                    <h2>
-                        Tax
-                        <span className='currency'>$</span>
-                        <span className='amount'>
-                            {tax.toFormat('0,0.00')}
-                        </span>
-                    </h2>
-                </Link>
-                <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
-                    <h2>
-                        Tip
-                        <span className='currency'>$</span>
-                        <span className='amount'>
-                            {tip.toFormat('0,0.00')}
-                        </span>
-                    </h2>
-                </Link>
-                <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
-                    <h2>
-                        Fees
-                        <span className='currency'>$</span>
-                        <span className='amount'>
-                            {fees.toFormat('0,0.00')}
-                        </span>
-                    </h2>
-                </Link>
-                <div className='bill-totals__total'>
-                    <h2>
-                        Total
-                        <span className='currency'>$</span>
-                        <span className='amount'>
-                            {total.toFormat('0,0.00')}
-                        </span>
-                    </h2>
+    if(currentBill) {
+        const subtotal = calculateBillSubtotal(currentBill);
+        const discounts = Money({ amount: (Number(currentBill.discounts)*100), currency});
+        const tax = Money({ amount: (Number(currentBill.tax)*100), currency});
+        const tip = Money({ amount: (Number(currentBill.tip)*100), currency});
+        const fees = Money({ amount: (Number(currentBill.fees)*100), currency});
+        const total = subtotal.add(tax).add(tip).add(fees).subtract(discounts);
+        
+        return (
+            <div className='bill-totals'>
+                <div className='col-1'>
+                    <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
+                        <h2>
+                            Discounts
+                            <span className='currency'>$</span>
+                            <span className='amount'>
+                                {discounts.toFormat('0,0.00')}
+                            </span>
+                        </h2>
+                    </Link>
+                </div>
+                <div className='col-2'>
+                    <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
+                        <h2>
+                            Tax
+                            <span className='currency'>$</span>
+                            <span className='amount'>
+                                {tax.toFormat('0,0.00')}
+                            </span>
+                        </h2>
+                    </Link>
+                    <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
+                        <h2>
+                            Tip
+                            <span className='currency'>$</span>
+                            <span className='amount'>
+                                {tip.toFormat('0,0.00')}
+                            </span>
+                        </h2>
+                    </Link>
+                    <Link className='bill-totals__edit' to={`/bills/${currentBill.id}/edit`}>
+                        <h2>
+                            Fees
+                            <span className='currency'>$</span>
+                            <span className='amount'>
+                                {fees.toFormat('0,0.00')}
+                            </span>
+                        </h2>
+                    </Link>
+                    <div className='bill-totals__total'>
+                        <h2>
+                            Total
+                            <span className='currency'>$</span>
+                            <span className='amount'>
+                                {total.toFormat('0,0.00')}
+                            </span>
+                        </h2>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return <></>
+    }
 }
 
 export default React.memo(BillTotals);
